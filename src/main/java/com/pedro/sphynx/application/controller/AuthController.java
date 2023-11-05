92472d6650cfc5c6ef666491bc936249bf0c423a
Pedro <pedrohvidals@gmail.com>
2023-11-05 12:36:09 -0300

    [fix] - upgrading the token initial validation

--- src/main/java/com/pedro/sphynx/application/controller/AuthController.java
+++ src/main/java/com/pedro/sphynx/application/controller/AuthController.java
@@ -3,9 +3,10 @@ package com.pedro.sphynx.application.controller;
 import com.pedro.sphynx.application.dtos.auth.UserDataLoginInputDTO;
 import com.pedro.sphynx.application.dtos.auth.UserDataOutputLoginDTO;
 import com.pedro.sphynx.application.dtos.auth.UserDataVerifyInputDTO;
-import com.pedro.sphynx.application.dtos.auth.UserDataVerifyOutput;
+import com.pedro.sphynx.application.dtos.auth.UserDataVerifyOutputDTO;
 import com.pedro.sphynx.domain.TokenService;
 import com.pedro.sphynx.infrastructure.entities.User;
+import com.pedro.sphynx.infrastructure.repository.UserRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -23,6 +24,9 @@ public class AuthController {
     @Autowired
     private TokenService tokenService;
 
+    @Autowired
+    private UserRepository userRepository;
+
     @PostMapping
     public ResponseEntity login(@RequestBody @Valid UserDataLoginInputDTO data){
         var token = new UsernamePasswordAuthenticationToken(data.user(), data.password());
@@ -34,8 +38,12 @@ public class AuthController {
 
     @PostMapping("/verify")
     public ResponseEntity verify(@RequestBody @Valid UserDataVerifyInputDTO data){
-        boolean valid = tokenService.isValid(data.token());
+        String subject = tokenService.getSubject(data.token());
 
-        return ResponseEntity.ok(new UserDataVerifyOutput(data.token(), valid));
+        if(userRepository.existsByUser(subject)){
+            return ResponseEntity.ok(new UserDataVerifyOutputDTO(data.token(), true));
+        } else{
+            return ResponseEntity.ok(new UserDataVerifyOutputDTO(data.token(), false));
+        }
     }
 }
