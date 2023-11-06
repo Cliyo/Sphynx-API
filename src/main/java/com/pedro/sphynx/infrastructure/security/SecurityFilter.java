c03b9683f8fae19afc42745754fa30041d418afa
Pedro <pedrohvidals@gmail.com>
2023-11-06 10:24:07 -0300

    [feat] - creating a authentication validation with a filter

---
+++ src/main/java/com/pedro/sphynx/infrastructure/security/SecurityFilter.java
@@ -0,0 +1,55 @@
+package com.pedro.sphynx.infrastructure.security;
+
+import com.pedro.sphynx.domain.TokenService;
+import com.pedro.sphynx.infrastructure.repository.UserRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.antlr.v4.runtime.Token;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private TokenService tokenService;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var tokenJWT = getToken(request);
+
+        if(tokenJWT != null){
+            var subject = tokenService.getSubject(tokenJWT);
+
+            var user = userRepository.findByUser(subject);
+
+            var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String getToken(HttpServletRequest request) {
+        var authHeader = request.getHeader("Authorization");
+
+        System.out.println(authHeader);
+
+        if(authHeader != null){
+            return authHeader.replace("Bearer ", "");
+        }
+
+        return null;
+    }
+}
