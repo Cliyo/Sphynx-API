d185faf95405a670931a867ba9e10cc39f6ed630
Pedro <pedrohvidals@gmail.com>
2023-11-08 01:55:09 -0300

    [feat] - creating a validation in the consumer creation

--- src/main/java/com/pedro/sphynx/domain/ConsumerService.java
+++ src/main/java/com/pedro/sphynx/domain/ConsumerService.java
@@ -5,6 +5,7 @@ import com.pedro.sphynx.application.dtos.consumer.ConsumerDataEditInputDTO;
 import com.pedro.sphynx.application.dtos.consumer.ConsumerDataInputDTO;
 import com.pedro.sphynx.infrastructure.entities.Consumer;
 import com.pedro.sphynx.infrastructure.repository.ConsumerRepository;
+import jakarta.validation.ValidationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,6 +18,16 @@ public class ConsumerService {
     private ConsumerRepository repository;
 
     private ResourceBundle messages = ResourceBundle.getBundle("messages");
+
+    public ConsumerDataComplete createVerify(ConsumerDataInputDTO data){
+        if(repository.existsByRa(data.ra())){
+            throw new RuntimeException(messages.getString("error.raAlreadyExists"));
+        } else{
+            Consumer consumer = new Consumer(data);
+            repository.save(consumer);
+            return new ConsumerDataComplete(consumer);
+        }
+    }
 
     public ConsumerDataComplete updateVerify(ConsumerDataEditInputDTO data, String ra){
         if(repository.existsByRa(ra)){
