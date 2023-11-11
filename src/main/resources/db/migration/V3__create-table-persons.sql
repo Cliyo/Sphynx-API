2e2c5db241cb63da6a11b1de610edf5d57c69f00
Pedro <pedrohvidals@gmail.com>
2023-11-11 14:21:20 -0300

    [fix] - creating the persons table in database

---
+++ src/main/resources/db/migration/V3__create-table-persons.sql
@@ -0,0 +1,8 @@
+create table persons(
+    id bigint not null auto_increment,
+    ra varchar(255) not null,
+    name varchar(255) not null,
+    gender varchar(255) not null,
+
+    primary key(id)
+);
