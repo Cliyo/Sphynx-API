442ff2e2c8e765b53cc184d0d932ba5c28edbdb3
Pedro <pedrohvidals@gmail.com>
2023-11-14 09:58:25 -0300

    [feat] - modifying consumer dto to accept the name parameter

--- src/main/java/com/pedro/sphynx/application/dtos/consumer/ConsumerDataComplete.java
+++ src/main/java/com/pedro/sphynx/application/dtos/consumer/ConsumerDataComplete.java
@@ -2,8 +2,13 @@ package com.pedro.sphynx.application.dtos.consumer;
 
 import com.pedro.sphynx.infrastructure.entities.Consumer;
 
-public record ConsumerDataComplete (Long id, String ra, String tag){
+public record ConsumerDataComplete (Long id, String ra, String name, String tag){
+
     public ConsumerDataComplete(Consumer consumer){
-        this(consumer.getId(), consumer.getRa(), consumer.getTag());
+        this(consumer.getId(), consumer.getRa(), null, consumer.getTag());
+    }
+
+    public ConsumerDataComplete withName(ConsumerDataComplete consumer, String name){
+        return new ConsumerDataComplete(consumer.id, consumer.ra, name, consumer.tag);
     }
 }
