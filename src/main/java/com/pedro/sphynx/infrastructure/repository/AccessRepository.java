fa897557e07052af726d27c50b128c82c30d08a4
Pedro <pedrohvidals@gmail.com>
2023-11-15 10:15:45 -0300

    [feat] - creating functions in access repository to filter result

--- src/main/java/com/pedro/sphynx/infrastructure/repository/AccessRepository.java
+++ src/main/java/com/pedro/sphynx/infrastructure/repository/AccessRepository.java
@@ -3,6 +3,12 @@ package com.pedro.sphynx.infrastructure.repository;
 import com.pedro.sphynx.infrastructure.entities.Access;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface AccessRepository extends JpaRepository<Access, Long> {
     Access findOneByRa(String ra);
+
+    List<Access> findAllByRa(String ra);
+
+    List<Access> findAllByLocal(String local);
 }
