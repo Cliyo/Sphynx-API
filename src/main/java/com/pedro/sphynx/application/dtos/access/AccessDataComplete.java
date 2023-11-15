fc1aff39bb17786a24399cbf0cefb0b0e00a3a0d
Pedro <pedrohvidals@gmail.com>
2023-11-15 12:16:57 -0300

    [feat] - adding tag field in AccessDataComplete DTO

--- src/main/java/com/pedro/sphynx/application/dtos/access/AccessDataComplete.java
+++ src/main/java/com/pedro/sphynx/application/dtos/access/AccessDataComplete.java
@@ -4,12 +4,12 @@ import com.pedro.sphynx.infrastructure.entities.Access;
 
 import java.time.LocalDateTime;
 
-public record AccessDataComplete(Long id, String ra, String name, String local, LocalDateTime date) {
+public record AccessDataComplete(Long id, String ra, String tag, String name, String local, LocalDateTime date) {
     public AccessDataComplete(Access data){
-        this(data.getId(), data.getRa(), null, data.getLocal(), data.getDate());
+        this(data.getId(), data.getRa(), data.getTag(), null, data.getLocal(), data.getDate());
     }
 
     public AccessDataComplete withName(AccessDataComplete access, String name){
-        return new AccessDataComplete(access.id, access.ra, name, access.local, access.date);
+        return new AccessDataComplete(access.id, access.ra, access.tag, name, access.local, access.date);
     }
 }
