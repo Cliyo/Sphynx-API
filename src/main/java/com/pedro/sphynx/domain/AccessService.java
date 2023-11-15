f01f4c99980c37339a4642544fb0cd461e4bb455
Pedro <pedrohvidals@gmail.com>
2023-11-15 10:16:36 -0300

    [feat] - creating "RA is in person table" validation in access creation

--- src/main/java/com/pedro/sphynx/domain/AccessService.java
+++ src/main/java/com/pedro/sphynx/domain/AccessService.java
@@ -6,6 +6,7 @@ import com.pedro.sphynx.infrastructure.entities.Access;
 import com.pedro.sphynx.infrastructure.entities.Consumer;
 import com.pedro.sphynx.infrastructure.repository.AccessRepository;
 import com.pedro.sphynx.infrastructure.repository.ConsumerRepository;
+import com.pedro.sphynx.infrastructure.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -21,6 +22,9 @@ public class AccessService {
     @Autowired
     private ConsumerRepository consumerRepository;
 
+    @Autowired
+    private PersonRepository personRepository;
+
     private ResourceBundle messages = ResourceBundle.getBundle("messages");
 
     public void validateCreation(AccessDataInput data){
@@ -28,12 +32,16 @@ public class AccessService {
             throw new RuntimeException(messages.getString("error.tagDontExists"));
         }
 
+        ConsumerDataComplete consumer = new ConsumerDataComplete(consumerRepository.findByTag(data.tag()));
+        if(!personRepository.existsByRa(consumer.ra())){
+            throw new RuntimeException(messages.getString("error.raDontExistsInPerson"));
+        }
+
         //validacao para ver se o local realmente existe no banco de dados
 
         //validacao se o consumer pode acessar o lugar, ainda falta criar tabela para os locais,
         //adicionar coluna de permissao no consumer
 
-        var consumer = new ConsumerDataComplete(consumerRepository.findByTag(data.tag()));
         var access = new Access(null, data.tag(), consumer.ra(), data.local(), LocalDateTime.now());
 
         accessRepository.save(access);
