5f19cec36771f7fc8e5a6257e9e7f02e5a49cefc
PedroVidalDev <pedrohvidals@gmail.com>
2023-12-14 11:06:22 -0300

    [refactor] - creating a function to verify the existence of a local in database

--- src/main/java/com/pedro/sphynx/infrastructure/repository/LocalRepository.java
+++ src/main/java/com/pedro/sphynx/infrastructure/repository/LocalRepository.java
@@ -4,4 +4,5 @@ import com.pedro.sphynx.infrastructure.entities.Local;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LocalRepository extends JpaRepository<Local, Long> {
+    boolean existsByName(String local);
 }
