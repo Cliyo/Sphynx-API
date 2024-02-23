10cf04e38ec6ff038c86e57ca8ab5513721f8ee0
PedroVidalDev <pedrohvidals@gmail.com>
2024-02-22 22:36:17 -0300

    [fix] - adjusting table name of access entity

--- src/main/java/com/pedro/sphynx/infrastructure/entities/Access.java
+++ src/main/java/com/pedro/sphynx/infrastructure/entities/Access.java
@@ -8,7 +8,7 @@ import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
-@Table(name = "accessregisters")
+@Table(name = "accessRegisters")
 @Entity(name = "Access")
 @Getter
 @NoArgsConstructor
