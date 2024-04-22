7e2a7dc071d5bf017a5bab7ba3c0527999fa6a7f
PedroVidalDev <pedrohvidals@gmail.com>
2024-04-22 19:12:05 -0300

    feat: adding validations in local creation method

--- src/main/java/com/pedro/sphynx/domain/LocalService.java
+++ src/main/java/com/pedro/sphynx/domain/LocalService.java
@@ -4,6 +4,7 @@ import com.pedro.sphynx.application.dtos.local.LocalDataComplete;
 import com.pedro.sphynx.application.dtos.local.LocalDataEditInput;
 import com.pedro.sphynx.application.dtos.local.LocalDataInput;
 import com.pedro.sphynx.infrastructure.entities.Local;
+import com.pedro.sphynx.infrastructure.exceptions.Validation;
 import com.pedro.sphynx.infrastructure.repository.LocalRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -30,14 +31,21 @@ public class LocalService {
     public LocalDataComplete createVerify(LocalDataInput data, String language){
         defineMessagesLanguage(language);
 
-        if(!repository.existsByName(data.name()) || !repository.existsByMac(data.mac())){
+        if(repository.existsByName(data.name())){
+            throw new Validation(messages.getString("error.localAlreadyExists"));
+        }
+
+        if(repository.existsByMac(data.mac())){
+            throw new Validation(messages.getString("error.macAlreadyExists"));
+        }
+
+        else{
             Local local = new Local(data);
 
             repository.save(local);
 
             return new LocalDataComplete(local);
         }
-        return null;
     }
 
     public LocalDataComplete updateVerify(LocalDataEditInput data, String name, String language) {
