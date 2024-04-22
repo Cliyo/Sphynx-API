5874221eb2a7dd880da1884f1704ccf7619034ef
PedroVidalDev <pedrohvidals@gmail.com>
2024-04-22 20:49:19 -0300

    feat: adding permission data in complete consumer and local dto

--- src/main/java/com/pedro/sphynx/application/dtos/consumer/ConsumerDataComplete.java
+++ src/main/java/com/pedro/sphynx/application/dtos/consumer/ConsumerDataComplete.java
@@ -3,9 +3,9 @@ package com.pedro.sphynx.application.dtos.consumer;
 import com.pedro.sphynx.application.dtos.person.PersonDataComplete;
 import com.pedro.sphynx.infrastructure.entities.Consumer;
 
-public record ConsumerDataComplete (Long id, PersonDataComplete person, String tag){
+public record ConsumerDataComplete (Long id, PersonDataComplete person, String tag, int permission){
 
     public ConsumerDataComplete(Consumer consumer){
-        this(consumer.getId(), new PersonDataComplete(consumer.getPerson()), consumer.getTag());
+        this(consumer.getId(), new PersonDataComplete(consumer.getPerson()), consumer.getTag(), consumer.getPermission());
     }
 }
--- src/main/java/com/pedro/sphynx/application/dtos/local/LocalDataComplete.java
+++ src/main/java/com/pedro/sphynx/application/dtos/local/LocalDataComplete.java
@@ -4,8 +4,8 @@ import com.pedro.sphynx.infrastructure.entities.Local;
 
 import java.time.LocalDateTime;
 
-public record LocalDataComplete(Long id, String name, String mac, LocalDateTime dtcreate, LocalDateTime dtupdate) {
+public record LocalDataComplete(Long id, String name, String mac, int permission, LocalDateTime dtcreate, LocalDateTime dtupdate) {
     public LocalDataComplete(Local data){
-        this(data.getId(), data.getName(), data.getMac(), data.getDtcreate(), data.getDtupdate());
+        this(data.getId(), data.getName(), data.getMac(), data.getPermission(), data.getDtcreate(), data.getDtupdate());
     }
 }
