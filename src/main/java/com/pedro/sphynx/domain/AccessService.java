de1bd8e06519dab29d883dcbbd686cff99271f66
PedroVidalDev <pedrohvidals@gmail.com>
2024-04-22 20:49:53 -0300

    feat: creating the validation if the consumer dont have permission to access that local

--- src/main/java/com/pedro/sphynx/domain/AccessService.java
+++ src/main/java/com/pedro/sphynx/domain/AccessService.java
@@ -3,7 +3,9 @@ package com.pedro.sphynx.domain;
 import com.pedro.sphynx.application.dtos.access.AccessDataComplete;
 import com.pedro.sphynx.application.dtos.access.AccessDataInput;
 import com.pedro.sphynx.application.dtos.consumer.ConsumerDataComplete;
+import com.pedro.sphynx.application.dtos.local.LocalDataComplete;
 import com.pedro.sphynx.infrastructure.entities.Access;
+import com.pedro.sphynx.infrastructure.entities.Person;
 import com.pedro.sphynx.infrastructure.exceptions.Validation;
 import com.pedro.sphynx.infrastructure.repository.AccessRepository;
 import com.pedro.sphynx.infrastructure.repository.ConsumerRepository;
@@ -47,20 +49,22 @@ public class AccessService {
         if(!consumerRepository.existsByTag(data.tag())){
             throw new Validation(messages.getString("error.tagDontExists"));
         }
-
         ConsumerDataComplete consumer = new ConsumerDataComplete(consumerRepository.findByTag(data.tag()));
+
         if(!personRepository.existsByRa(consumer.person().ra())){
             throw new Validation(messages.getString("error.raDontExistsInPerson"));
         }
 
-        if(!localRepository.existsByName(data.local())){
+        if(!localRepository.existsByMac(data.local())){
             throw new Validation(messages.getString("error.localDontExists"));
         }
+        LocalDataComplete local = new LocalDataComplete(localRepository.findByMac(data.local()));
 
-        //validacao se o consumer pode acessar o lugar, ainda falta criar tabela para os locais,
-        //adicionar coluna de permissao no consumer
+        if(local.permission() > consumer.permission()){
+            throw new Validation(messages.getString("error.consumerDontHavePermission"));
+        }
 
-        var access = new Access(null, consumerRepository.findByTag(data.tag()), localRepository.findByName(data.local()), LocalDateTime.now());
+        var access = new Access(null, consumerRepository.findByTag(data.tag()), localRepository.findByName(data.local()), true, LocalDateTime.now());
 
         accessRepository.save(access);
 
