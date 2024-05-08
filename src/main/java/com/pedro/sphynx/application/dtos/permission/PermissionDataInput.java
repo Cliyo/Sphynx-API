e3d76a94fe783b1339914c0cdc5909ac571afc6f
PedroVidalDev <pedrohvidals@gmail.com>
2024-05-07 22:25:42 -0300

    feat: adding validations in dto

--- src/main/java/com/pedro/sphynx/application/dtos/permission/PermissionDataInput.java
+++ src/main/java/com/pedro/sphynx/application/dtos/permission/PermissionDataInput.java
@@ -1,4 +1,9 @@
 package com.pedro.sphynx.application.dtos.permission;
 
-public record PermissionDataInput (int level, String name){
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record PermissionDataInput (
+        @NotNull Integer level,
+        @NotBlank String name){
 }
