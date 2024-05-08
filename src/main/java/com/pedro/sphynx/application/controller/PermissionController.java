1a598c5d600dfc4cb2fc064b227ddd360608423a
PedroVidalDev <pedrohvidals@gmail.com>
2024-05-07 21:02:45 -0300

    feat: adding the create, delete and get methods in permissions

--- src/main/java/com/pedro/sphynx/application/controller/PermissionController.java
+++ src/main/java/com/pedro/sphynx/application/controller/PermissionController.java
@@ -1,13 +1,36 @@
 package com.pedro.sphynx.application.controller;
 
+import com.pedro.sphynx.application.dtos.consumer.ConsumerDataComplete;
+import com.pedro.sphynx.application.dtos.message.MessageDTO;
 import com.pedro.sphynx.application.dtos.permission.PermissionDataComplete;
 import com.pedro.sphynx.application.dtos.permission.PermissionDataInput;
+import com.pedro.sphynx.domain.MessageService;
+import com.pedro.sphynx.domain.PermissionService;
+import com.pedro.sphynx.infrastructure.repository.PermissionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("permissions")
 public class PermissionController implements ControllerIN<PermissionDataInput, PermissionDataComplete> {
+
+    @Autowired
+    private PermissionRepository repository;
+
+    @Autowired
+    private PermissionService service;
+
+    @Autowired
+    private MessageService messageService;
+
     @Override
     public ResponseEntity create(PermissionDataInput data, String language) {
-        return null;
+        var local = service.createVerify(data, language);
+        MessageDTO dto = messageService.createMessage(201, local, language);
+
+        return ResponseEntity.ok(dto);
     }
 
     @Override
@@ -16,12 +39,16 @@ public class PermissionController implements ControllerIN<PermissionDataInput, P
     }
 
     @Override
-    public ResponseEntity delete(String id) {
-        return null;
+    public ResponseEntity delete(String name) {
+        repository.deleteByName(name);
+
+        return ResponseEntity.noContent().build();
     }
 
     @Override
     public ResponseEntity get() {
-        return null;
+        var listPermissions = repository.findAll().stream().map(PermissionDataComplete::new).toList();
+
+        return ResponseEntity.ok(listPermissions);
     }
 }
