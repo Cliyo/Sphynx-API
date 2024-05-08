abfd4e8f00cb7b8b56a85a2ed9546a856826ffe8
PedroVidalDev <pedrohvidals@gmail.com>
2024-05-07 21:02:55 -0300

    feat: creating permissions dto

--- src/main/java/com/pedro/sphynx/application/dtos/permission/PermissionDataComplete.java
+++ src/main/java/com/pedro/sphynx/application/dtos/permission/PermissionDataComplete.java
@@ -1,4 +1,9 @@
 package com.pedro.sphynx.application.dtos.permission;
 
+import com.pedro.sphynx.infrastructure.entities.Permission;
+
 public record PermissionDataComplete(int level, String name) {
+    public PermissionDataComplete(Permission data){
+        this(data.getLevel(), data.getName());
+    }
 }
