34822420985f0c6f499f11fe714135fad99736a4
PedroVidalDev <pedrohvidals@gmail.com>
2024-05-07 21:03:16 -0300

    feat: creating methods to permissions in database

--- src/main/java/com/pedro/sphynx/infrastructure/repository/PermissionRepository.java
+++ src/main/java/com/pedro/sphynx/infrastructure/repository/PermissionRepository.java
@@ -4,4 +4,9 @@ import com.pedro.sphynx.infrastructure.entities.Permission;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PermissionRepository extends JpaRepository<Permission, Integer> {
+    void deleteByName(String name);
+
+    boolean existsByLevel(int level);
+
+    boolean existsByName(String name);
 }
