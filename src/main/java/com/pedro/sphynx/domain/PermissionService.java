382c50a137e0349ff454130a0e85ced2995d05bd
PedroVidalDev <pedrohvidals@gmail.com>
2024-05-07 21:03:30 -0300

    feat: creating validations to create a new permission in project

--- src/main/java/com/pedro/sphynx/domain/PermissionService.java
+++ src/main/java/com/pedro/sphynx/domain/PermissionService.java
@@ -1,4 +1,36 @@
 package com.pedro.sphynx.domain;
 
+import com.pedro.sphynx.application.dtos.consumer.ConsumerDataComplete;
+import com.pedro.sphynx.application.dtos.consumer.ConsumerDataInput;
+import com.pedro.sphynx.application.dtos.permission.PermissionDataComplete;
+import com.pedro.sphynx.application.dtos.permission.PermissionDataInput;
+import com.pedro.sphynx.infrastructure.entities.Consumer;
+import com.pedro.sphynx.infrastructure.entities.Permission;
+import com.pedro.sphynx.infrastructure.exceptions.Validation;
+import com.pedro.sphynx.infrastructure.repository.PermissionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.ResourceBundle;
+
+import static com.pedro.sphynx.domain.utils.LanguageService.defineMessagesLanguage;
+
+@Service
 public class PermissionService {
+
+    @Autowired
+    private PermissionRepository repository;
+
+    public PermissionDataComplete createVerify(PermissionDataInput data, String language){
+        ResourceBundle messages = defineMessagesLanguage(language);
+
+        if(repository.existsByLevel(data.level()) || repository.existsByName(data.name())){
+            throw new Validation(messages.getString("error.permissionAlreadyExists"));
+        }
+
+        else{
+            Permission permission = new Permission(data);
+            repository.save(permission);
+            return new PermissionDataComplete(permission);
+        }
+    }
 }
