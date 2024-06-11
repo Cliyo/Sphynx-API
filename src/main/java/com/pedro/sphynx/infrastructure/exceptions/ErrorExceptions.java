69ec7969a6f8d8ce39a790766184b52436b7c57d
PedroVidalDev <pedrohvidals@gmail.com>
2024-06-11 18:25:24 -0300

    feat: adding error exception to login

--- src/main/java/com/pedro/sphynx/infrastructure/exceptions/ErrorExceptions.java
+++ src/main/java/com/pedro/sphynx/infrastructure/exceptions/ErrorExceptions.java
@@ -3,6 +3,7 @@ package com.pedro.sphynx.infrastructure.exceptions;
 import com.pedro.sphynx.application.dtos.message.MessageDTO;
 import jakarta.persistence.EntityNotFoundException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -24,4 +25,10 @@ public class ErrorExceptions {
     public ResponseEntity validation(Validation e){
         return ResponseEntity.badRequest().body(new MessageDTO(400, e.getMessage(), null));
     }
+
+    @ExceptionHandler(UsernameNotFoundException.class)
+    public ResponseEntity auth(UsernameNotFoundException e){
+        return ResponseEntity.status(401).body(new MessageDTO(401, "Usuario ou senha incorretos.", null));
+    }
+
 }
