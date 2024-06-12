fde6f21d1ec2b63758cdca1f8315dab10a2ce7ee
PedroVidalDev <pedrohvidals@gmail.com>
2024-06-12 00:09:56 -0300

    feat: creating one function in consumer repository to filter then by permission

--- src/main/java/com/pedro/sphynx/infrastructure/repository/ConsumerRepository.java
+++ src/main/java/com/pedro/sphynx/infrastructure/repository/ConsumerRepository.java
@@ -3,6 +3,8 @@ package com.pedro.sphynx.infrastructure.repository;
 import com.pedro.sphynx.infrastructure.entities.Consumer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ConsumerRepository extends JpaRepository<Consumer, Long> {
 
     boolean existsByTag(String tag);
@@ -14,4 +16,6 @@ public interface ConsumerRepository extends JpaRepository<Consumer, Long> {
     Consumer getReferenceByRa(String ra);
 
     void deleteByRa(String ra);
+
+    List<Consumer> findAllByPermission_Level(Integer level);
 }
