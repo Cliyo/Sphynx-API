bf8f2e2e5686fd941ad6e1dac9fcb519eaeb042a
PedroVidalDev <pedrohvidals@gmail.com>
2024-06-26 16:28:32 -0300

    fix: adjusting the foreign keys names

---
+++ src/main/resources/db/migration/V8__create-table-consumers-groups.sql
@@ -0,0 +1,9 @@
+create table if not exists consumers_permissions(
+    id bigint not null auto_increment,
+    consumer_id bigint not null,
+    group_id bigint not null,
+
+    primary key(id),
+    constraint fk_permissions_consumer_id foreign key (consumer_id) references consumers(id),
+    constraint fk_permissions_group_id foreign key (group_id) references permissionGroups(id)
+);
