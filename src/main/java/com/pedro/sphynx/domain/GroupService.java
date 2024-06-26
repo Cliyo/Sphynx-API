a46f2cd83ce52ca95c493fb11c3bcd8bc40bf5c8
PedroVidalDev <pedrohvidals@gmail.com>
2024-06-26 16:53:25 -0300

    feat: creating the function to create a new group

--- src/main/java/com/pedro/sphynx/domain/GroupService.java
+++ src/main/java/com/pedro/sphynx/domain/GroupService.java
@@ -2,6 +2,8 @@ package com.pedro.sphynx.domain;
 
 import com.pedro.sphynx.application.dtos.group.GroupDataComplete;
 import com.pedro.sphynx.application.dtos.group.GroupDataInput;
+import com.pedro.sphynx.infrastructure.entities.Group;
+import com.pedro.sphynx.infrastructure.exceptions.Validation;
 import com.pedro.sphynx.infrastructure.repository.GroupRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -18,10 +20,18 @@ public class GroupService {
     public GroupDataComplete createVerify(GroupDataInput data, String language){
         ResourceBundle messages = defineMessagesLanguage(language);
 
-        return null;
+        if(repository.existsByName(data.name())){
+            throw new Validation(messages.getString("error.groupAlreadyExists"));
+        }
+
+        Group group = new Group(data);
+
+        repository.save(group);
+
+        return new GroupDataComplete(group);
     }
 
-    public void deleteVerify(String level, String language) {
+    public void deleteVerify(Long id, String language) {
         ResourceBundle messages = defineMessagesLanguage(language);
 
 
