3d917f60817ca126493a385f3c23a7fe54a50249
PedroVidalDev <pedrohvidals@gmail.com>
2024-06-26 16:28:19 -0300

    fix: adding one line in migration to define id for primary key

--- src/main/resources/db/migration/V7__create-table-groups.sql
+++ src/main/resources/db/migration/V7__create-table-groups.sql
@@ -1,6 +1,8 @@
-create table if not exists permissions(
+create table if not exists permissionGroups(
     id bigint not null auto_increment,
     name varchar(255) not null,
     dtcreate datetime not null,
-    dtupdate datetime
+    dtupdate datetime,
+
+    primary key(id)
 );
