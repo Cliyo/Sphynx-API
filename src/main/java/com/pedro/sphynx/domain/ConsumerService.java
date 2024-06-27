9ad9940394e415e717836b8d38ae75d1c57275ca
PedroVidalDev <pedrohvidals@gmail.com>
2024-06-27 15:12:05 -0300

    feat: saving a new consumer and consumerGroup in database

--- src/main/java/com/pedro/sphynx/domain/ConsumerService.java
+++ src/main/java/com/pedro/sphynx/domain/ConsumerService.java
@@ -4,9 +4,11 @@ import com.pedro.sphynx.application.dtos.consumer.ConsumerDataComplete;
 import com.pedro.sphynx.application.dtos.consumer.ConsumerDataEditInput;
 import com.pedro.sphynx.application.dtos.consumer.ConsumerDataInput;
 import com.pedro.sphynx.infrastructure.entities.Consumer;
+import com.pedro.sphynx.infrastructure.entities.ConsumerGroup;
 import com.pedro.sphynx.infrastructure.entities.Group;
 import com.pedro.sphynx.infrastructure.exceptions.Validation;
 import com.pedro.sphynx.infrastructure.repository.ConsumerRepository;
+import com.pedro.sphynx.infrastructure.repository.ConsumerGroupRepository;
 import com.pedro.sphynx.infrastructure.repository.GroupRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -24,6 +26,9 @@ public class ConsumerService{
     @Autowired
     private GroupRepository groupRepository;
 
+    @Autowired
+    private ConsumerGroupRepository consumerGroupRepository;
+
     public ConsumerDataComplete createVerify(ConsumerDataInput data, String language){
         ResourceBundle messages = defineMessagesLanguage(language);
 
@@ -31,9 +36,18 @@ public class ConsumerService{
             throw new Validation(messages.getString("error.raAlreadyExists"));
         }
 
+        if(groupRepository.existsByName(data.permission())){
+            throw new Validation(messages.getString("error.groupNotExists"));
+        }
+
         else{
             Consumer consumer = new Consumer(data);
+            Group group = groupRepository.getReferenceByName(data.permission());
+            ConsumerGroup consumerGroup = new ConsumerGroup(null, consumer, group);
+
             consumerRepository.save(consumer);
+            consumerGroupRepository.save(consumerGroup);
+
             return new ConsumerDataComplete(consumer);
         }
     }
