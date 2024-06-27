88db52a45f6ae42dfa572b33563d98f8031ca79c
PedroVidalDev <pedrohvidals@gmail.com>
2024-06-27 15:58:25 -0300

    feat: creating table to have the local permissions and fix the fk names

--- src/main/resources/db/migration/V8__create-table-consumers-groups.sql
+++ src/main/resources/db/migration/V8__create-table-consumers-groups.sql
@@ -5,5 +5,5 @@ create table if not exists consumers_groups(
 
     primary key(id),
     constraint fk_permissions_consumer_id foreign key (consumer_id) references consumers(id),
-    constraint fk_permissions_group_id foreign key (group_id) references permissionGroups(id)
+    constraint fk_permissionsLocal_group_id foreign key (group_id) references permissionGroups(id)
 );
