95e2dd1c53ca0b644118221e571fcd218d7fbab9
PedroVidalDev <pedrohvidals@gmail.com>
2024-07-12 18:16:39 -0300

    fix: fixing the migrations

---
+++ src/main/resources/db/migration/V8__create-table-locals-groups.sql
@@ -0,0 +1,9 @@
+create table if not exists locals_groups(
+    id bigint not null auto_increment,
+    local_id bigint not null,
+    group_id bigint not null,
+
+    primary key(id),
+    constraint fk_permissions_local_id foreign key (local_id) references locals(id),
+    constraint fk_permissionsConsumer_group_id foreign key (group_id) references permissionGroups(id)
+);
