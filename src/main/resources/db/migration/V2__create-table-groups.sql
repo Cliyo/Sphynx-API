7e670de825969d9c746888d7e5e6443445b99ce8
PedroVidalDev <pedrohvidals@gmail.com>
2024-07-26 13:59:14 -0300

    refactor: change the groups table name

--- src/main/resources/db/migration/V2__create-table-groups.sql
+++ src/main/resources/db/migration/V2__create-table-groups.sql
@@ -1,8 +1,9 @@
-create table if not exists permissiongroups(
+create table if not exists permissionGroups(
     id bigint not null auto_increment,
     name varchar(255) not null,
     dtcreate datetime not null,
     dtupdate datetime,
 
     primary key(id)
+
 );
