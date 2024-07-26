929a1e122853daedbac400cff566896d437fe83b
Snootic <snooticonoob@hotmail.com>
2024-07-26 19:32:52 -0300

    feat: transfrom groups name into unique key

--- src/main/resources/db/migration/V2__create-table-groups.sql
+++ src/main/resources/db/migration/V2__create-table-groups.sql
@@ -1,6 +1,6 @@
 create table if not exists permission_groups(
     id bigint not null auto_increment,
-    name varchar(255) not null,
+    name varchar(255) not null UNIQUE,
     dtcreate datetime not null,
     dtupdate datetime,
 
