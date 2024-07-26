d31781db70d8d19c4b4f89ece254288f910369be
Snootic <snooticonoob@hotmail.com>
2024-07-26 19:33:21 -0300

    feat: add cascade delete on locals_groups for locals

--- src/main/resources/db/migration/V8__create-table-locals-groups.sql
+++ src/main/resources/db/migration/V8__create-table-locals-groups.sql
@@ -4,6 +4,6 @@ create table if not exists locals_groups(
     group_id bigint not null,
 
     primary key(id),
-    constraint fk_permissions_local_id foreign key (local_id) references locals(id),
+    constraint fk_permissions_local_id foreign key (local_id) references locals(id) ON DELETE CASCADE,
     constraint fk_permissionsConsumer_group_id foreign key (group_id) references permission_groups(id)
 );
