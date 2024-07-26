7c9aec85a2f04d3c14853a02158210d8218a48c3
Snootic <snooticonoob@hotmail.com>
2024-07-26 20:46:39 -0300

    feat: transform mac address from locals into unique key

--- src/main/resources/db/migration/V3__create-table-locals.sql
+++ src/main/resources/db/migration/V3__create-table-locals.sql
@@ -1,7 +1,7 @@
 create table if not exists locals(
     id bigint not null auto_increment,
     name varchar(255) not null,
-    mac varchar(255) not null,
+    mac varchar(255) not null UNIQUE,
     dtcreate datetime not null,
     dtupdate datetime,
 
