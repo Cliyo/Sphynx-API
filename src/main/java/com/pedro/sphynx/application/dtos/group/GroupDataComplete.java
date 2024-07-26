22e20d93c33fc21f7a8b5e17d2c81fd6c972ce93
Snootic <snooticonoob@hotmail.com>
2024-07-26 17:52:47 -0300

    refactor: add id on group data

--- src/main/java/com/pedro/sphynx/application/dtos/group/GroupDataComplete.java
+++ src/main/java/com/pedro/sphynx/application/dtos/group/GroupDataComplete.java
@@ -2,8 +2,8 @@ package com.pedro.sphynx.application.dtos.group;
 
 import com.pedro.sphynx.infrastructure.entities.Group;
 
-public record GroupDataComplete(String name) {
+public record GroupDataComplete(Long id, String name) {
     public GroupDataComplete(Group data){
-        this(data.getName());
+        this(data.getId(),data.getName());
     }
 }
