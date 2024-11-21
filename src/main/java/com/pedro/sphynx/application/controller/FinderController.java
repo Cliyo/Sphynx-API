d5227e55596f786b90768c55cf99829854113b39
Snootic <snooticonoob@hotmail.com>
2024-11-20 21:30:01 -0300

    feat: add FinderController get endpoint to retrieve devices from multicast service

---
+++ src/main/java/com/pedro/sphynx/application/controller/FinderController.java
@@ -0,0 +1,27 @@
+package com.pedro.sphynx.application.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pedro.sphynx.domain.MulticastService;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+@RequestMapping("deviceFinder")
+public class FinderController {
+
+    @Autowired
+    private MulticastService multicastService;
+
+    @GetMapping
+    public ResponseEntity<List<List<String>>> getDevices() {
+        List<List<String>> devices = multicastService.getDevices();
+        return ResponseEntity.ok(devices);
+    }
+}
