f423323bc428e41a38bca4e9dfa6c1d9102eacd8
Snootic <snooticonoob@hotmail.com>
2024-11-20 22:09:06 -0300

    feat: add scan endpoint to FinderController for manual device scanning

--- src/main/java/com/pedro/sphynx/application/controller/FinderController.java
+++ src/main/java/com/pedro/sphynx/application/controller/FinderController.java
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
 @RestController
 @RequestMapping("deviceFinder")
 public class FinderController {
@@ -24,4 +23,12 @@ public class FinderController {
         List<List<String>> devices = multicastService.getDevices();
         return ResponseEntity.ok(devices);
     }
+
+    @GetMapping("scan")
+    public ResponseEntity<List<List<String>>> scanDevices() {
+        multicastService.finderScan();
+        List<List<String>> devices = multicastService.getDevices();
+        return ResponseEntity.ok(devices);
+    }
+    
 }
