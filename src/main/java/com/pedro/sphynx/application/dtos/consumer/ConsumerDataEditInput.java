92f97843d488d02913acc392d8d29ce37b3b0788
PedroVidalDev <pedrohvidals@gmail.com>
2025-03-12 21:19:31 -0300

    feat: add format validations in consumer dtos

--- src/main/java/com/pedro/sphynx/application/dtos/consumer/ConsumerDataEditInput.java
+++ src/main/java/com/pedro/sphynx/application/dtos/consumer/ConsumerDataEditInput.java
@@ -1,8 +1,15 @@
 package com.pedro.sphynx.application.dtos.consumer;
 
+import jakarta.validation.constraints.Pattern;
+
 public record ConsumerDataEditInput(
         String name,
+
+        @Pattern(regexp="\\d{13}")
         String ra,
+
+        @Pattern(regexp="([A-Z]{2} \\d{1,2} ?)+")
         String tag,
+
         Integer group)
 {}
--- src/main/java/com/pedro/sphynx/application/dtos/consumer/ConsumerDataInput.java
+++ src/main/java/com/pedro/sphynx/application/dtos/consumer/ConsumerDataInput.java
@@ -2,10 +2,20 @@ package com.pedro.sphynx.application.dtos.consumer;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
 public record ConsumerDataInput(
-        @NotBlank String name,
-        @NotBlank String ra,
-        @NotBlank String tag,
-        @NotNull Integer group){
+        @NotBlank
+        String name,
+
+        @NotBlank
+        @Pattern(regexp="\\d{13}")
+        String ra,
+
+        @NotBlank
+        @Pattern(regexp="([A-Z]{2} \\d{1,2} ?)+")
+        String tag,
+
+        @NotNull
+        Integer group){
 }
