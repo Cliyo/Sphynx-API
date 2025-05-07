eb260f0e7b0330e2d655b5acf6cd77db6720e842
PedroVidalDev <pedrohvidals@gmail.com>
2025-05-07 10:51:13 -0300

    refactor: add more fields in access return dto

--- src/main/java/com/pedro/sphynx/dtos/access/AccessDataComplete.java
+++ src/main/java/com/pedro/sphynx/dtos/access/AccessDataComplete.java
@@ -4,10 +4,12 @@ import com.pedro.sphynx.dtos.consumer.ConsumerDataComplete;
 import com.pedro.sphynx.dtos.local.LocalDataComplete;
 import com.pedro.sphynx.entities.Access;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 
-public record AccessDataComplete(Long id, Boolean status, ConsumerDataComplete consumer, LocalDataComplete local, LocalDateTime date) {
+public record AccessDataComplete(Long id, Boolean status, ConsumerDataComplete consumer, LocalDataComplete local, LocalDate date, LocalTime time) {
     public AccessDataComplete(Access data){
-        this(data.getId(), data.getStatus(),new ConsumerDataComplete(data.getConsumer()), new LocalDataComplete(data.getLocal()), data.getDate());
+        this(data.getId(), data.getStatus(),new ConsumerDataComplete(data.getConsumer()), new LocalDataComplete(data.getLocal()), data.getDate().toLocalDate(), data.getDate().toLocalTime());
     }
 }
