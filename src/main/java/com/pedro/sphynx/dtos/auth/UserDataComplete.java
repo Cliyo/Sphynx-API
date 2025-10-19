f2f0116f0e4ee6b73dab2b4d3bc2699fa73bda60
Snootic <snooticonoob@gmail.com>
2025-10-19 20:31:09 -0300

    refactor: add Group to UserDataComplete

--- src/main/java/com/pedro/sphynx/dtos/auth/UserDataComplete.java
+++ src/main/java/com/pedro/sphynx/dtos/auth/UserDataComplete.java
@@ -1,9 +1,10 @@
 package com.pedro.sphynx.dtos.auth;
 
+import com.pedro.sphynx.dtos.group.GroupDataComplete;
 import com.pedro.sphynx.dtos.unit.UnitDataComplete;
 import com.pedro.sphynx.entities.User;
 
-public record UserDataComplete (Long id, String name, String ra, String tag, String user, UserDataComplete userCreator, UnitDataComplete unit){
+public record UserDataComplete (Long id, String name, String ra, String tag, String user, GroupDataComplete group, UserDataComplete userCreator, UnitDataComplete unit){
 
     public UserDataComplete(User user){
         this(
@@ -12,6 +13,11 @@ public record UserDataComplete (Long id, String name, String ra, String tag, Str
             user.getRa(), 
             user.getTag() != null ? user.getTag() : null, 
             user.getUser(), 
+            user.getGroup() != null ?
+                new GroupDataComplete(
+                    user.getGroup()
+                ) : null
+            ,
             user.getUserCreator() != null ? 
                 new UserDataComplete(
                     user.getUserCreator()
