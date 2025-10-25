210bfb21e4aa609405743929d22bf8a8504b7a46
PedroVidalDev <pedrohvidals@gmail.com>
2025-10-24 22:09:54 -0300

    fix: remove debug print statement in UserService

--- src/main/java/com/pedro/sphynx/services/UserService.java
+++ src/main/java/com/pedro/sphynx/services/UserService.java
@@ -109,7 +109,6 @@ public class UserService {
             }
             userCreated.setGroup(groupRepository.getReferenceById(data.groupId()));
         } else if (loggedUser.isAdmin()) {
-            System.out.println("ENTROU AQUI");
             userCreated.setGroup(groupRepository.save(new Group(new GroupDataInput(data.ra() + " - Grupo Padrão", null), userCreated)));
         }
         userCreated = userRepository.save(userCreated);
